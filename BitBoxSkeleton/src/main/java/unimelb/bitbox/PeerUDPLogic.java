465da332e00c07211db14601c7753bc60ab2bc25
elfsong <dumingzhe@126.com>
2019-05-23 23:54:34 +1000

    Add infinity timeout on client server

--- BitBoxSkeleton/src/main/java/unimelb/bitbox/PeerUDPLogic.java
+++ BitBoxSkeleton/src/main/java/unimelb/bitbox/PeerUDPLogic.java
@@ -59,6 +59,7 @@ public class PeerUDPLogic extends Thread {
             byte[] data = new byte[packageSize];
             DatagramPacket receivedPacket = new DatagramPacket(data, data.length);
             try {
+                datagramSocket.setSoTimeout(0);
                 datagramSocket.receive(receivedPacket);
                 if (!receivedPacket.getAddress().equals(InetAddress.getByName(localIp))) {
                     handleLogic(datagramSocket, receivedPacket);
